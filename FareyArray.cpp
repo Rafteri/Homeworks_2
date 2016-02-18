7fefc08c650afa8f8f634b1355dba4dba7953ebd
Rafteri <raaf98@mail.ru>
2016-02-18 08:42:53 +0300

    Better code

--- FareyArray.cpp
+++ FareyArray.cpp
@@ -1,30 +1,36 @@
 //
-// Created by Rafteri on 16.02.2016.
+// Created by Rafteri on 18.02.2016.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
 struct Rational {
     int a, b;
 };
 
-void Farey(int n){
+void print(Rational num[], int k){
+    for (int i = 0;  i < k ; i++){
+        cout << num[i].a << '/' << num[i].b << endl;
+    }
+}
 
+Rational* Farey(int n, int &k){
     const int size = 10000;
-    struct Rational num[size];
-    int i, k = 2;
+    Rational* num = new Rational[size];
+    k = 2;
 
     num[0].a = 0;
     num[0].b = 1;
 
     num[1].a = 1;
     num[1].b = 1;
-    for (i = 0; i < n + 1; i++) {
+
+    for (int i = 0; i < n + 1; i++) {
         int j = 0;
         while (j < k - 1) {
             if ((num[j].b + num[j + 1].b) == i) {
-                int l;
-                for (l = k + 1; l > j + 1; l--) {
+                for (int l = k + 1; l > j + 1; l--) {
                     num[l] = num[l - 1];
                 }
                 num[j + 1].a = num[j].a + num[j + 2].a;
@@ -36,15 +42,13 @@ void Farey(int n){
             j++;
         }
     }
-
-    for (i = 0;  i < k ; i++){
-            printf("%d/%d \n", num[i].a, num[i].b);
-        }
+    return num;
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    Farey(n);
+    int n, k;
+    cin >> n;
+    Rational* num = Farey(n, k);
+    print(num, k);
     return 0;
 }
