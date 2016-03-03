48a785336706473a835ac5ed56984a8c5daabda5
Rafteri <raaf98@mail.ru>
2016-03-03 18:32:18 +0300

    Homework

--- List_1.cpp
+++ List_1.cpp
@@ -3,105 +3,105 @@
 //
 
 #include <iostream>
+
 using namespace std;
 
-struct Node{
-        char character;
-        Node *next = NULL;
+struct Node {
+    char character;
+    Node *next = NULL;
 };
 
-struct LinkedList{
+struct LinkedList {
     Node *first = NULL;
     Node *last = NULL;
-    void AddToEnd(char c){
-            if (first == NULL){
-                    Node *node = new Node;
-                    node->character = c;
-                    first = node;
-                    last = node;
-                    node->next = NULL;
-            }
-            else {
-                    Node *node = new Node;
-                    node->character = c;
-                    last->next = node;
-                    last = node;
-                    node->next = NULL;
-            }
 
-    }
-    void ReadList(){
-            string s;
-            getline(cin, s);
-            for (int i = 0; i < s.length(); i++){
-                    AddToEnd(s[i]);
-            }
-    }
-    void PrintList(){
-            Node *node = first;
-            while (node != NULL){
-                    cout << node->character;
-                    node = node->next;
-            }
-    }
 };
 
-bool IsChar(char c){
-        //return (((c > 64)&&(c < 91)) || ((c > 96)&&(c < 123)) || ((c > 127)&&(c < 176)) || ((c > 223)&&(c < 242))); //Why code doesn't see rus characters?
-        return (((c >= 64)&&(c < 91)) || ((c > 96)&&(c < 123)));
+void AddToEnd(LinkedList *list, char c) {
+    Node *node = new Node;
+    node->character = c;
+    node->next = NULL;
+    if (list->first == NULL) {
+        list->first = node;
+        list->last = node;
+    }
+    else {
+        list->last->next = node;
+        list->last = node;
+    }
+
 }
 
-LinkedList *FoundMaxWord(LinkedList *text, int *curlength){
-        LinkedList *MaxWord = new LinkedList;
-        int length = 0;
-        Node *node = text->first;
-        Node *wnode; //word node
-        Node *CurWord; // current word
+void ReadList(LinkedList *list) {
+    string s;
+    getline(cin, s);
+    for (int i = 0; i < s.length(); i++) {
+        AddToEnd(list, s[i]);
+    }
+}
 
-        while (node != NULL){
-                if (IsChar(node->character))
-                {
-                        CurWord = node;
-                        while (node != NULL && IsChar(node->character) ) {
-                                length++;
-                                node = node->next;
-                        }
-                        if (length > *curlength) {
+void PrintList(LinkedList *list) {
+    Node *node = list->first;
+    while (node != NULL) {
+        cout << node->character;
+        node = node->next;
+    }
+}
 
-                                *curlength = length;
-                                wnode = MaxWord->first;
-                                do {
-                                        if (wnode == NULL) {
-                                                MaxWord->AddToEnd(CurWord->character);
-                                                CurWord = CurWord->next;
-                                        }
-                                        else {
-                                                wnode->character = CurWord->character;
-                                                CurWord = CurWord->next;
-                                                wnode = wnode->next;
-                                        }
-                                } while (CurWord != node);
-                        }
-                        length = 0;
-                }
-                else {
-                        node = node->next;
-                }
+bool IsChar(char c) {
+    return (((c >= 64) && (c < 91)) || ((c > 96) && (c < 123)));
+}
+
+LinkedList *FoundMaxWord(LinkedList *text, int *curlength) {
+    LinkedList *MaxWord = new LinkedList;
+    int length = 0;
+    Node *node = text->first;
+    Node *wnode; //word node
+    Node *CurWord; // current word
+
+    while (node != NULL) {
+        if (IsChar(node->character)) {
+            CurWord = node;
+            while (node != NULL && IsChar(node->character)) {
+                length++;
+                node = node->next;
+            }
+            if (length > *curlength) {
+
+                *curlength = length;
+                wnode = MaxWord->first;
+                do {
+                    if (wnode == NULL) {
+                        AddToEnd(MaxWord, CurWord->character);
+                        CurWord = CurWord->next;
+                    }
+                    else {
+                        wnode->character = CurWord->character;
+                        CurWord = CurWord->next;
+                        wnode = wnode->next;
+                    }
+                } while (CurWord != node);
+            }
+            length = 0;
         }
-        return MaxWord;
+        else {
+            node = node->next;
+        }
+    }
+    return MaxWord;
 }
 
-int main(){
-        LinkedList text;
-        text.ReadList();
-        int length = 0;
+int main() {
+    LinkedList *text = new LinkedList;
+    ReadList(text);
+    int length = 0;
 
-        LinkedList *MaxWord = FoundMaxWord(&text, &length);
+    LinkedList *MaxWord = FoundMaxWord(text, &length);
 
-        MaxWord->PrintList();
-        cout << endl << "Length of MaxWord: " << length;
+    PrintList(MaxWord);
+    cout << endl << "Length of MaxWord: " << length;
 
-        return 0;
+    return 0;
 }
 
 
